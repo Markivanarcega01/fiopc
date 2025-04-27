d6d356db41120723e9da2d0bb20d011c5059fddc
Markivanarcega01 <arcegamarkivan@gmail.com>
2025-04-27 14:06:18 +0800

    Update: Env created, Mysql Db connected

--- fi_opc/settings.py
+++ fi_opc/settings.py
@@ -77,10 +77,18 @@ WSGI_APPLICATION = 'fi_opc.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    },
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fiopc_db',
+        'USER': 'root',
+        'PASSWORD': 'Markivan01',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
 }
 
 
