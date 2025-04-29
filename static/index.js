d45fb27f5406118acddf55c0d5f41d3d0e6a715c
Ian Carl Sarmiento <sarmiento.iancarl20@gmail.com>
2025-04-29 15:24:42 +0800

    Upload: careers modal

--- static/index.js
+++ static/index.js
@@ -1,8 +1,10 @@
+// navigation bar burger menu
 function toggleMenu(x) {
   x.classList.toggle("change");
   document.querySelector('.nav-links-mobile').classList.toggle('show');
 }
 
+// navigation bar links
 function toggleMenu(element) {
   element.classList.toggle("active");
 
@@ -10,6 +12,17 @@ function toggleMenu(element) {
   nav.classList.toggle("show");
 }
 
+// navigation bar links
 function toggleCollapse(element) {
   element.parentElement.classList.toggle('active');
+}
+
+// careers page modal
+function openModal() {
+  document.getElementById("modalOverlay").style.display = "flex";
+}
+
+// careers page modal close button
+function closeModal() {
+  document.getElementById("modalOverlay").style.display = "none";
 }
