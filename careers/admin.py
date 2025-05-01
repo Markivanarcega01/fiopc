a61b21499f0639f8b3d372dfc85c0f160e28e99a
Ian Carl Sarmiento <sarmiento.iancarl20@gmail.com>
2025-05-01 13:41:59 +0800

    Update: Careers page

--- careers/admin.py
+++ careers/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Application  # Import your model
 
-# Register your models here.
+admin.site.register(Application)
--- careers/models.py
+++ careers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Application(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    additional_info = models.TextField(blank=True)
+    cv = models.FileField(upload_to='cvs/')
+
+    def __str__(self):
+        return self.full_name
