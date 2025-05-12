61db526e7846243105656daa5e4caeed42fd7017
Markivanarcega01 <arcegamarkivan@gmail.com>
2025-05-12 18:27:24 +0800

    Update: Path and display of services

--- services/urls.py
+++ services/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = 'services'
 urlpatterns = [
     path('',views.index, name='index'),
+    path('consulting_services/',views.display_consulting_services, name='consulting_services'),
+    path('rehabilitation/',views.display_rehabilitation, name='rehabilitation'),
+    path('technology_upgrade/',views.display_technology_upgrade, name='technology_upgrade'),
+    path('engineering_services/',views.display_engineering_services, name='engineering_services'),
 ]
--- services/views.py
+++ services/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "services/services.html")
+
+def display_consulting_services(request):
+    return render(request, "services/consulting_services.html")
+
+def display_engineering_services(request):
+    return render(request, "services/engineering_services.html")
+
+def display_rehabilitation(request):
+    return render(request, "services/rehabilitation.html")
+
+def display_technology_upgrade(request):
+    return render(request, "services/technology_upgrade.html")
