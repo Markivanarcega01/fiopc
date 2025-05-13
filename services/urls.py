fec9f5dfb058f037a04bae8c0f0e3a2c143983fd
Ian Carl Sarmiento <sarmiento.iancarl20@gmail.com>
2025-05-13 16:56:39 +0800

    Upgrade: finished services categories

--- services/urls.py
+++ services/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'services'
 urlpatterns = [
     path('',views.index, name='index'),
-    path('consulting_services/',views.display_consulting_services, name='consulting_services'),
+    path('consulting-services/',views.display_consulting_services, name='consulting-services'),
     path('rehabilitation/',views.display_rehabilitation, name='rehabilitation'),
-    path('technology_upgrade/',views.display_technology_upgrade, name='technology_upgrade'),
-    path('engineering_services/',views.display_engineering_services, name='engineering_services'),
+    path('technology-upgrade/',views.display_technology_upgrade, name='technology-upgrade'),
+    path('engineering-services/',views.display_engineering_services, name='engineering-services'),
+    path('operations-and-maintenance/',views.display_operations_and_maintenance, name='operations-and-maintenance'),
+    path('design-and-build/',views.display_design_and_build, name='design-and-build'),
 ]
--- services/views.py
+++ services/views.py
@@ -5,13 +5,19 @@ def index(request):
     return render(request, "services/services.html")
 
 def display_consulting_services(request):
-    return render(request, "services/consulting_services.html")
+    return render(request, "services/consulting-services.html")
 
 def display_engineering_services(request):
-    return render(request, "services/engineering_services.html")
+    return render(request, "services/engineering-services.html")
 
 def display_rehabilitation(request):
     return render(request, "services/rehabilitation.html")
 
 def display_technology_upgrade(request):
-    return render(request, "services/technology_upgrade.html")
+    return render(request, "services/technology-upgrade.html")
+
+def display_operations_and_maintenance(request):
+    return render(request, "services/operations-and-maintenance.html")
+
+def display_design_and_build(request):
+    return render(request, "services/design-and-build.html")
